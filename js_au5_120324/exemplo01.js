bfb1a251881f606c65d19a55932cf405e8dc019b
gustavfaustino <gustav.faustino107@gmail.com>
2024-03-12 09:50:46 -0300

    Parte 01 da aula

---
+++ js_au5_120324/exemplo01.js
@@ -0,0 +1,7 @@
+var nome = "Luis"
+
+if (nome == "Luis"){
+    var idade = 40
+}
+
+console.log(idade)
---
+++ js_au5_120324/exemplo05.js
@@ -0,0 +1,17 @@
+let valores
+let valorB
+let resultado
+
+//entry
+valorA = 30
+valorB =20
+
+valorC=15
+valorD = 40
+//processing
+resultado = valorB + valorA
+resultado2= valorC + valorD
+
+//exit
+console.log(resultado)
+console.log(resultado2)
