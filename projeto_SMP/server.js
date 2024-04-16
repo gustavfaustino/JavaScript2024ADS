6fdbeae9730de32fa95fed0d680b11b6ffab1c50
Gustavo Gabriel <gustav.faustino107@gmail.com>
2024-04-16 08:40:50 -0300

    Delete projeto_SMP directory

--- projeto_SMP/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-
-app.set('port',2312) // Usar uma porta livre, n utilizavel. 8080 n pode
-const server = app.listen(app.get('port'),()=>{
-    console.log("Servidor rodando na porta: "+ server.address().port)
-})
-
